6bb720752eab2644b1ec58517ead59118524cc4c
sopleee <leesophie99@gmail.com>
2022-08-09 17:34:14 -0400

    Began header
    wrote header- who are you

---
+++ ocd-app/src/Verify.svelte
@@ -0,0 +1,28 @@
+<script>
+    import { Router, Route, Link } from "svelte-navigator";
+</script>
+
+<!-- <div id="spacer">
+    <p>
+        
+    </p>
+</div> -->
+
+<div id="top">
+    <h1>Who Are You?</h1>
+</div>
+
+<div>
+
+</div>
+
+
+<style>
+    #top {
+        position:absolute;
+
+    }
+    /* #spacer {
+        height:500 px
+    } */
+</style>
