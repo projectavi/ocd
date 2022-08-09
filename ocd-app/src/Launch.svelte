3d86ddaf2a9f257fb91bad8aad3ac83883e96730
Aviraj Newatia <projectavi.exe@gmail.com>
2022-08-09 14:51:43 -0700

    Logo image and dropdown

--- ocd-app/src/Launch.svelte
+++ ocd-app/src/Launch.svelte
@@ -1,14 +1,15 @@
 <script>
     import { Router, Route, Link } from "svelte-navigator";
-    import { Button } from "sveltestrap";
+    import { Button, Dropdown, DropdownItem, DropdownMenu, DropdownToggle } from 'sveltestrap';
 </script>
     
     <div id="signinButton">
         <Button size="lg">Sign In</Button>
     </div>
 
-    <div id="center">
+    <div id="center" class="row-align">
         <div>
+            <img id="logo" src="https://www.transparentpng.com/thumb/wallet/77VFRH-white-wallet-photos.png" alt="white wallet photos @transparentpng.com">
         </div>
 
         <div>
@@ -17,6 +18,18 @@
             </h1>
             <h3> On-Chain Certifications in your pocket </h3>
         </div>
+    </div>
+
+    <div id="register" class="row-align">
+        <h2 id="margin-right"> I am a </h2>
+        <Dropdown>
+            <DropdownToggle caret> Choose an option </DropdownToggle>
+            <DropdownMenu>
+                <DropdownItem header> I am a... </DropdownItem>
+                <DropdownItem> an organisation </DropdownItem>
+                <DropdownItem> an individual </DropdownItem>
+            </DropdownMenu>
+        </Dropdown>
     </div>
 
 <style>
@@ -35,4 +48,28 @@
         transform: translate(-50%, -50%);
     }
 
+    #register {
+        /* Center at the bottom */
+        position: absolute;
+        top: 85%;
+        left: 40%;
+        right: 40%;
+    }
+
+    #margin-right
+    {
+        margin-right: 20px;
+    }
+
+    #logo {
+        scale: 0.5;
+        margin-right: -50px;
+    }
+
+    .row-align {
+        /* Layout horizontally */
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+    }
 </style>
