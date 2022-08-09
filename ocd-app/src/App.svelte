1f6391e24cb7ea844a04fc32fb5b22278531a18e
Aviraj Newatia <projectavi.exe@gmail.com>
2022-08-09 13:29:20 -0700

    Set up routing

--- ocd-app/src/App.svelte
+++ ocd-app/src/App.svelte
@@ -1,30 +1,25 @@
 <script>
-	export let name;
+	import { Router, Route, Link } from "svelte-navigator";
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<Router>
+	<header>
+		<h1>
+			On-Chain Degrees
+		</h1>
+	</header>
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+	<main>
+		<Route path="launch">
+			<Launch />
+		</Route>
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+		<Route path="verify">
+			<Verify />
+		</Route>
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+		<Route path="wallet">
+			<Wallet />
+		</Route>
+	</main>
+</Router>
