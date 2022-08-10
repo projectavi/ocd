d5cfd1c3092da9d37c1ee01b13eff7319034988f
sopleee <leesophie99@gmail.com>
2022-08-09 21:44:16 -0400

    some styling, recoloring, resizing
    made section titles smaller, made left navigation pink

--- ocd-app/src/Verify.svelte
+++ ocd-app/src/Verify.svelte
@@ -1,9 +1,6 @@
 <script>
     import { Router, Route, Link } from "svelte-navigator";
-
-    function clickselect() {
-        
-}
+    
 </script>
 
 <div id="top">
@@ -11,24 +8,24 @@
 </div>
 
 <div id="static-left">
-    <h5 style="text-decoration: none;">
-        <a href="#scan">
-            <strong style='font-size:60px'>1</strong> Scan
+    <h5>
+        <a href="#scan" class="nav">
+            <strong style='font-size:40px'>1</strong> Scan
         </a>
     </h5>
     <h5>
-        <a href="#summary">
-            <strong style='font-size:60px'>2</strong> Summary
+        <a href="#summary" class="nav">
+            <strong style='font-size:40px'>2</strong> Summary
         </a>
     </h5>
     <h5>
-        <a href="#linkwallet">
-            <strong style='font-size:60px'>3</strong> Link Wallet
+        <a href="#linkwallet" class="nav">
+            <strong style='font-size:40px'>3</strong> Link Wallet
         </a>
     </h5>
     <h5>
-        <a href="#launching">
-            <strong style='font-size:60px'>3</strong> Launching..
+        <a href="#launching" class="nav">
+            <strong style='font-size:40px'>4</strong> Launching..
         </a>
     </h5>
 </div>
@@ -36,23 +33,23 @@
 
 <div id="scroll-right">
     <section id="scan">
-        <h1 style="font-size:100px">
+        <h1 style="font-size:70px">
             Scan
         </h1>
     </section>
     <section id="summary">
-        <h1 style="font-size:100px">
+        <h1 style="font-size:70px">
             Summary
         </h1>
     </section>
     <section id="linkwallet">
-        <h1 style="font-size:100px">
+        <h1 style="font-size:70px">
             Link Wallet
         </h1>
         <p></p>
     </section>
     <section id="launching">
-        <h1 style="font-size:100px">
+        <h1 style="font-size:70px">
             Launching..
         </h1>
         <p></p>
@@ -79,8 +76,13 @@
     #static-left{
         position: sticky;
         float: left;
-        top: 35%;
+        top: 40%;
         margin-left:7%;
         top: 2rem;
+    }
+
+    .nav{
+        text-decoration: none;
+        color:#FF5D88;
     }
 </style>
