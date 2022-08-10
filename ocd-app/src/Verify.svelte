ffd43d04a84c38b8b9d8dbd41de1b3aaf5b953c6
sopleee <leesophie99@gmail.com>
2022-08-09 20:51:20 -0400

    Nav bar with smooth scrolling
    added smooth scrolling style when click section on nav bar, it scrolls to the respective section on the form sheet

--- ocd-app/src/Verify.svelte
+++ ocd-app/src/Verify.svelte
@@ -1,5 +1,9 @@
 <script>
     import { Router, Route, Link } from "svelte-navigator";
+
+    function clickselect() {
+        
+}
 </script>
 
 <div id="top">
@@ -7,78 +11,76 @@
 </div>
 
 <div id="static-left">
-    <h5>
-        <strong style='font-size:60px'>1</strong> Scan
+    <h5 style="text-decoration: none;">
+        <a href="#scan">
+            <strong style='font-size:60px'>1</strong> Scan
+        </a>
     </h5>
     <h5>
-        <strong style='font-size:60px'>2</strong> Summary
+        <a href="#summary">
+            <strong style='font-size:60px'>2</strong> Summary
+        </a>
     </h5>
     <h5>
-        <strong style='font-size:60px'>3</strong> Link Wallet
+        <a href="#linkwallet">
+            <strong style='font-size:60px'>3</strong> Link Wallet
+        </a>
+    </h5>
+    <h5>
+        <a href="#launching">
+            <strong style='font-size:60px'>3</strong> Launching..
+        </a>
     </h5>
 </div>
 
 
 <div id="scroll-right">
-    <p>
-        heyo
-    </p>
-    <br>
-    <h1 style="font-size:160px">
-        SLDKFJLKS
-    </h1>
-    <br>
-    <h1 style="font-size:160px">
-        SLDKFJLKS
-    </h1>
-    <br>
-    <h1 style="font-size:160px">
-        SLDKFJLKS
-    </h1>
-    <br>
-    <h1 style="font-size:160px">
-        SLDKFJLKS
-    </h1>
-    <br>
-    <h1 style="font-size:160px">
-        SLDKFJLKS
-    </h1>
-    <br>
-    <h1 style="font-size:160px">
-        SLDKFJLKS
-    </h1>
-    <br>
-    <h1 style="font-size:160px">
-        SLDKFJLKS
-    </h1>
+    <section id="scan">
+        <h1 style="font-size:100px">
+            Scan
+        </h1>
+    </section>
+    <section id="summary">
+        <h1 style="font-size:100px">
+            Summary
+        </h1>
+    </section>
+    <section id="linkwallet">
+        <h1 style="font-size:100px">
+            Link Wallet
+        </h1>
+        <p></p>
+    </section>
+    <section id="launching">
+        <h1 style="font-size:100px">
+            Launching..
+        </h1>
+        <p></p>
+    </section>
 </div>
 
 
-
-
 <style>
+    html{
+        scroll-behavior: smooth;
+    }
     #top {
-        position: sticky;
         top: 10%;
         margin-left: 7%;
-        background-color: blue;
-        /* transform: translate(-50%, -50%); */
+        height:60%;
     }
     #scroll-right {
         position: relative;
-        background-color: purple;
-        /* overflow: auto; */
-        margin-top:15%;
+        overflow: auto;
+        margin-top:2%;
         margin-left:30%;
-        width:65%;
     }
 
     #static-left{
         position: sticky;
-        background-color: orange;
         float: left;
         top: 35%;
         margin-left:7%;
+        top: 2rem;
     }
-
 </style>
