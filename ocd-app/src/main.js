9eeff6e4c1f5b25ca1e63741ca4393006526f562
khalid0md <khalid.abdallas.here@gmail.com>
2022-08-09 21:21:33 -0400

    ok2

--- ocd-app/src/main.js
+++ ocd-app/src/main.js
@@ -1,5 +1,6 @@
 import App from './App.svelte';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { ContractFactory, ethers } from "./ethers-5.6.esm.min.js";
 
 const app = new App({
 	target: document.body,
@@ -8,11 +9,37 @@ const app = new App({
 	}
 });
 
-const Web3 = require('web3');
-const url = "https://rpc.ankr.com/eth";
+const signinButton = document.getElementById("signinButton")
+signinButton.onclick = signin()
 
-provider.eth.getBlockNumber().then((result) => {
-	console.log("Current block number: " + result);
-});
+async function signin() {
+	if (typeof window.ethereum !== "undefined") {
+		try {
+			await window.ethereum.request({ method: "eth_requestAccounts" })
+		}
+		catch (error) {
+			console.log(error)
+		}
+		signinButton.innerHTML = "Connected!"
+		const accounts = await ethereum.request({ method: "eth_accounts" })
+		const provider = new ethers.providers.Web3Provider(window.ethereum)
+		const signer = provider.getSigner()
+		console.log(ethers)
+		console.log("hi")
+		//console.log(error)
+	}
+	else {
+		signinButton.innerHTML = "Please install metamask connected!"
+	}
+}
+
+
+
+
+//const url = "https://rpc.ankr.com/eth";
+//const provider = new ethers.providers.JsonRpcProvider(url)
+//const signer = provider.getSigner()
+//console.log(signer);
+
 
 export default app;
